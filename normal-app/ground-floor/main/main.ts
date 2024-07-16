80673bab9ad0e296804fcd9c310528255a6eb6f6
jwkcs <jiwonkim652@gmail.com>
2024-07-16 17:29:18 +0100

    feat: normal app - ground floor - camera effect, displayRatio

---
+++ normal-app/ground-floor/main/main.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2022 ZEP Co., LTD
+ */
+
+import "zep-script";
+
+ScriptApp.onInit.Add(function() {
+    ScriptApp.cameraEffect = 1; // 1 = vignette effect
+	ScriptApp.cameraEffectParam1 = 650; // Sets the range of the vignette effect to 500
+    ScriptApp.displayRatio = 1.5;
+});
