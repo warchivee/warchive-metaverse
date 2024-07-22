e5d913ac3e82956ef04512b702cf79d68a270dff
jwkcs <jiwonkim652@gmail.com>
2024-07-22 09:23:37 +0100

    feat: disable map freeview

--- normal-app/ground-floor/main.ts
+++ normal-app/ground-floor/main.ts
@@ -6,11 +6,19 @@ import "zep-script";
 
 ScriptApp.onInit.Add(function() {
     ScriptApp.cameraEffect = 1; // 1 = vignette effect
-	ScriptApp.cameraEffectParam1 = 650; // Sets the range of the vignette effect to 500
-    ScriptApp.displayRatio = 1.5;
+	ScriptApp.cameraEffectParam1 = 500; // Sets the range of the vignette effect to 500
+    ScriptApp.displayRatio = 1;
+    //@ts-ignore
+    ScriptApp.enableFreeView = false;
 });
 
 ScriptApp.onJoinPlayer.Add(function(player){
 	player.hidden = true;
+    player.name = "첼";
+    player.disableVideo = true;
+    player.disableAudio = true;
+    player.showWidget("test.html", "top", 300, 300);
 	player.sendUpdated();
-})
+
+    player.showCenterLabel("지상 1층");
+});
